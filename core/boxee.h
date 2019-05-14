8ab40f598069b43ffb2a6ddd7ba2984b8f399f58
Raul Costa Junior <raulcostajunior@gmail.com>
2019-05-14 21:06:05 +0200

    Added some boxee operations and state data.

--- core/boxee.h
+++ core/boxee.h
@@ -2,29 +2,52 @@
 #define BOXEE_H
 
 #include <cstdint>
+#include <QString>
 
 namespace core {
-
-enum class BoxeeState : uint8_t {
-    OFF, // Powered off.
-    BOOTING,
-    ON_STANDARD,   // On and in standard mode (neither playing media nor handling text input).
-    ON_PLAYING,    // On and playing some kind of media.
-    ON_TEXT_ENTRY, // On and handling text input.
-    SHUTTING_DOWN
-};
-
-enum class BoxeeMediaType : uint8_t {
-    NONE, // No media playing (while not in playing mode).
-    AUDIO,
-    VIDEO,
-    IMAGE
-};
 
 class Boxee
 {
 public:
     static Boxee &instance();
+
+    enum class State : uint8_t {
+        OFF, // Powered off.
+        BOOTING,
+        ON_STANDARD,   // On and in standard mode (neither playing media nor handling text input).
+        ON_PLAYING,    // On and playing some kind of media.
+        ON_TEXT_ENTRY, // On and handling text input.
+        SHUTTING_DOWN
+    };
+
+    enum class MediaType : uint8_t {
+        NONE, // No media playing (while not in playing mode).
+        AUDIO,
+        VIDEO,
+        IMAGE
+    };
+
+    uint8_t bootTimeSecs() const { return _bootTimeSecs; }
+    void setBootTimeSecs(uint8_t);
+
+    uint8_t shutdownTimeSecs() const { return _shutdownTimeSecs; }
+    void setShutdownTimeSecs(uint8_t);
+
+    const QString &password() const { return _password; }
+    void setPassword(const QString &);
+
+    uint16_t httpPort() const { return _httpPort; }
+    void setHttpPort(uint16_t);
+
+    State state() const { return _state; }
+    void setState(State);
+
+    MediaType mediaType() const { return _mediaType; }
+    void setMediaType(MediaType);
+
+    void powerOn();
+
+    void powerOff();
 
 private:
     Boxee() = default;
@@ -34,6 +57,14 @@ private:
     Boxee &operator=(const Boxee &) = delete;
     Boxee(Boxee &&) = delete;
     Boxee &operator=(const Boxee &&) = delete;
+
+    State _state = State::OFF;
+    MediaType _mediaType = MediaType::NONE;
+
+    uint8_t _bootTimeSecs{3};
+    uint8_t _shutdownTimeSecs{2};
+    QString _password{""};
+    uint16_t _httpPort{8080};
 };
 
 } // namespace core
