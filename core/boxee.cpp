78f1878b9dff484c29cc689c4b07013a78c99909
Raul Costa Junior <raulcostajunior@gmail.com>
2019-05-26 09:58:32 +0200

    Boxee::powerOn() and Boxee:powerOff() defined.

--- core/boxee.cpp
+++ core/boxee.cpp
@@ -1,6 +1,7 @@
 #include "boxee.h"
 
 #include <QString>
+#include <QTimer>
 
 using namespace core;
 
@@ -62,10 +63,23 @@ void Boxee::setMediaType(MediaType mediaType)
 
 void Boxee::powerOn()
 {
-    // TODO: write body
+    if (_state != State::OFF) {
+        // Boxee is not turned off; ignore the powerOn command.
+        return;
+    }
+
+    setState(State::BOOTING);
+
+    QTimer::singleShot(_bootTimeSecs * 1000, [this] { setState(State::ON_STANDARD); });
 }
 
 void Boxee::powerOff()
 {
-    // TODO: write body
+    if (_state == State::OFF || _state == State::BOOTING || _state == State::SHUTTING_DOWN) {
+        return;
+    }
+
+    setState(State::SHUTTING_DOWN);
+
+    QTimer::singleShot(_shutdownTimeSecs * 1000, [this] { setState(State::OFF); });
 }
